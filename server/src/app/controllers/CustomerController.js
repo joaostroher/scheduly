954f4f94fe7f623be5b47a61f9a268b276542da1
João Miguel Ströher <joao.stroher@gmail.com>
2020-03-26 22:06:27 -0300

    Customer register (#4)
    * create_insert_customer
    
    Tentei criar a parte de inserir novo Cliente, mas não ta 100% ainda(nem perto), nem testei..
    
    Co-authored-by: stevanmesser <34461083+stevanmesser@users.noreply.github.com>

--- server/src/app/controllers/CustomerController.js
+++ server/src/app/controllers/CustomerController.js
@@ -1,9 +1,14 @@
-import Customers from '~/app/models/Customers';
+import Customer from '~/app/models/Customer';
 
 class CustomerController {
   async index(req, res) {
-    const customers = await Customers.find();
+    const customers = await Customer.find();
     return res.status(200).json(customers);
+  }
+
+  async post(req, res) {
+    const customer = await Customer.create(customer);
+    return customer;
   }
 }
 
--- web/src/App.js
+++ web/src/App.js
@@ -5,9 +5,12 @@ import { ThemeProvider } from 'styled-components';
 import theme from '~/theme';
 import Routes from '~/routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Routes />
       </ThemeProvider>
