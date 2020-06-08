64a8f07c20aeef56f4280dc4430d356ee6da963d
Jonatan Birck <jonatan.birck@universo.univates.br>
2020-06-07 21:48:05 -0300

    adjustments

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,8 +1,7 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-  //ORIGINAL - baseURL: 'http://localhost:3300/api/',
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'http://localhost:3300/api/',
 });
 
 export default api;
